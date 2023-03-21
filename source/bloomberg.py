6836335bd6a78eb7f15e1ad123787fba89086f9e
syalabi-seet <syalabi.seet@hotmail.com>
2023-03-21 08:43:01 +0800

    merge

--- source/bloomberg.py
+++ source/bloomberg.py
@@ -3,145 +3,116 @@ https://mengjiexu.com/post/bloomberg-api/
 https://capm.pe/bloomberg/
 """
 import os
-import copy
 import pandas as pd
-import regex as re
 
 from xbbg import blp
 from loguru import logger
 from tqdm.notebook import tqdm
 from glob import glob
 
+from itertools import islice
+from loguru import logger
+import time
 
-class BaseExtractor:
-    dir = "assets"
-    regions = ['EU', 'ASIA', 'OTHERS']  
-    flds = ['GHG_SCOPE_1', 'GHG_SCOPE_2', 'GHG_SCOPE_3'] 
+class BBHelper:
+    regions = ['Asia', 'Europe', 'Others']
+    fields = [
+        'GHG_SCOPE_1', 
+        'GHG_SCOPE_2',
+        'GHG_SCOPE_3',
+        'ENERGY_CONSUMPTION',
+        'TOTAL_WATER_USE'
+        'TOTAL_WASTE']
 
-    def __init__(self) -> None:
-        self._create_folders() 
-
-    def _get_tickers_by_region(self, region, flds):
-        src_path = os.path.join(BaseExtractor.dir, f"{flds}_{region}.xlsx")
-        self._ticker_df = pd.read_excel(src_path)
-        return self._ticker_df['Ticker'].unique().tolist()
-
-
-    def _create_folders(self):
-        for region in BaseExtractor.regions:
-            for fld in BaseExtractor.flds:
-                region_folder = os.path.join(BaseExtractor.dir, fld, region)
-                if not os.path.exists(region_folder):
-                    os.makedirs(region_folder, exist_ok=True)
-                    logger.info(f"{fld}/{region} folder created.")
-
-
-class BDH(BaseExtractor):
     def __init__(
-            self, region, flds, daily_limit=90, start_date='2011-01-01', 
-            end_date='2023-01-01', freq='Y'):
-        super().__init__()
-        assert region in BDH.regions, f"Please input region only from {BDH.regions}"
-        self._region = region
-        self._folder_dir = os.path.join(BDH.dir, region)
-        self._tickers = self._get_tickers_by_region(region, flds)
-        self._n_tickers = len(self._tickers)
-        self._remaining_tickers = self._get_remaining_tickers()
-        self._n_remaining_tickers = len(self._remaining_tickers)
-        self._flds = flds
-        self._n_flds = 1
-        self._daily_limit = daily_limit     
-        self._periods = pd.date_range(start=start_date, end=end_date, freq='Y')
-        self._years = [d.year for d in self._periods]
-        self._n_periods = len(self._periods)
-        self._cells_per_ticker = self._n_flds*self._n_periods
-        self._batch_size = self._get_batch_size()
-        self._days_remaining = (
-            self._n_remaining_tickers/self._batch_size if self._n_remaining_tickers>0 else 0)
-        self._start_date = start_date
-        self._end_date = end_date
-        self._freq = freq
-    
+            self, 
+            region, 
+            field,
+            cell_limit_per_request=1_000,
+            sleep_duration=60):      
 
-    def _get_batch_size(self):
-        a = self._daily_limit//self._cells_per_ticker
-        return a if self._n_remaining_tickers>a else self._n_remaining_tickers
-    
-
-    def __repr__(self):
-        return "\n".join([
-            "-----Summary-----",
-            f"region: {self._region}",
-            f"n_remaining_tickers: {self._n_remaining_tickers}/{self._n_tickers}",
-            f"flds: {self._flds}",
-            f"n_flds: {self._n_flds}",            
-            f"daily_limit: {self._daily_limit}",
-            f"cells_per_ticker: {self._cells_per_ticker}",
-            f"days_remaining: {self._days_remaining}",
-            f"batch_size: {self._batch_size}",
-            f"start_date: {self._start_date}",
-            f"end_date: {self._end_date}",
-            f"freq: {self._freq}",
-            f"n_periods: {self._n_periods}",
-        ])
-    
-
-    def _get_single_ticker_data(self, ticker):
-        return blp.bdh(
-            tickers=ticker,
-            flds=self._flds,
-            start_date=self._start_date,
-            end_date=self._end_date,
-            Per=self._freq,
-            Fill='NA')
-
-
-    def _get_remaining_tickers(self):
-        extracted_ticker_paths = glob(os.path.join(self._folder_dir, "*.csv"))
-        extracted_tickers = [
-            os.path.splitext(os.path.basename(ticker))[0] for ticker in extracted_ticker_paths]
-        remaining_tickers = copy.deepcopy(self._tickers)
-        for ticker in self._tickers:
-            temp_ticker = re.sub("(\/| )", "_", ticker)
-            if temp_ticker in extracted_tickers:
-                remaining_tickers.remove(ticker)
-        return remaining_tickers
-
-
-    def _get_save_path(self, ticker):
-        file_name = re.sub("(\/| )", "_", ticker) + ".csv"
-        return os.path.join(self._folder_dir, file_name)
+        assert region in BBHelper.regions, f"Select region within {BBHelper.regions}"
+        assert field in BBHelper.fields, f"Select field within {BBHelper.fields}"
+        self.region = region
+        self.field = field
+        self.time_period = len(pd.date_range("2010-01-01", "2023-01-01", freq="Y"))
+        self.cell_limit_per_request = cell_limit_per_request
+        self.ticker_limit_per_request = self.cell_limit_per_request // self.time_period
+        self.sleep_duration = sleep_duration
         
+        self.tickers = self._get_tickers()
+        self.data = self._get_saved_data()
+        self.sub_tickers = self._get_sub_tickers()
+        self.batch_tickers = self._get_batch(
+            self.sub_tickers, self.ticker_limit_per_request)
+        self._get_summary()
 
-    def fetch(self):
-        if not self._remaining_tickers:
-            # Early stop
-            logger.info("Region extracted.")
+
+    def _get_tickers(self):
+        self.dir = os.path.join("assets", "Bloomberg")
+        path = os.path.join(self.dir, "Snapshots", f"{self.field}.xlsx")
+        df = pd.read_excel(path, sheet_name=self.region)
+        return df['Ticker'].unique().tolist()
+
+
+    def _get_saved_data(self):
+        self.save_dir = os.path.join(self.dir, self.field)
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
+
+        self.save_path = os.path.join(self.save_dir, f"{self.region}.parquet")
+        if os.path.exists(self.save_path):
+            df = pd.read_parquet(self.save_path)
+        else:
+            df = pd.DataFrame(columns=['tic', 'datadate', self.field])
+        return df
+
+
+    def _get_sub_tickers(self):
+        self.retrieved_tickers = self.data['tic'].unique().tolist()
+        sub_tickers = [
+            i for i in self.tickers
+            if i not in self.retrieved_tickers]
+        return sub_tickers
+
+
+    def _get_batch(self, it, size):
+        it = iter(it)
+        return list(iter(lambda: tuple(islice(it, size)), ()))
+
+
+    def _get_summary(self):
+        print("=======================================")
+        print("Region:", self.region)
+        print("Cell limit per request:", self.cell_limit_per_request)
+        print("Ticker limit per request:", self.ticker_limit_per_request)
+        print("Tickers retrieved: {0}/{1}".format(len(self.retrieved_tickers), len(self.tickers)))
+        print("Batches:", len(self.batch_tickers))
+        print("Time periods:", self.time_period)
+        print("=======================================")
+
+
+    def extract(self):
+        if not self.batch_tickers:
+            logger.info(f"{self.region} completed")
             return
 
-        # Parse remaining tickers
-        not_empty_count = 0
-        for i, ticker in tqdm(
-                enumerate(self._remaining_tickers[:self._batch_size]), total=self._batch_size):
-            save_path = self._get_save_path(ticker)
-            if not os.path.exists(save_path):
-                try:
-                    row_data = self._get_single_ticker_data(ticker)
-                    row_data.to_csv(save_path)
-                    if not row_data.empty:
-                        not_empty_count+=1
-                        print(f"Not Empty: {not_empty_count}/{i+1}")
-                except:
-                    logger.info(f"Unable to save for {ticker} to {save_path}")
+        for tickers in tqdm(self.batch_tickers):
+            batch_data = blp.bdh(
+                tickers=tickers,
+                flds=self.field,
+                start_date="2010-01-01",
+                end_date="2023-01-01", 
+                Per="Y",
+                Fill="NA") 
+            for ticker in tickers:
+                if ticker not in batch_data.columns:
+                    logger.info(f"{ticker} not available")
+                    continue
+                sub_data = batch_data[ticker].reset_index(names="datadate")
+                sub_data['tic'] = ticker
+                sub_data = sub_data.dropna(subset=self.field)
+                self.data = pd.concat([self.data, sub_data])                 
 
-
-if __name__ == "__main__":
-    fn = BDH(
-        region='Europe',
-        flds=['GHG_SCOPE_1', 'GHG_SCOPE_2', 'GHG_SCOPE_3'],
-        daily_limit=90,
-        start_date='2010-01-01',
-        end_date='2020-01-01',
-        freq='Y')
-    print(fn)
-    fn.fetch()
+            self.data.to_parquet(self.save_path)
+            time.sleep(self.sleep_duration)
--- source/refinitiv.py
+++ source/refinitiv.py
@@ -11,6 +11,7 @@ from tqdm.notebook import tqdm
 from itertools import islice
 from loguru import logger
 
+
 ## Helper functions
 def match_years(file_path):
     save_path = os.path.splitext(file_path)[0] + "_matched.csv"
@@ -52,114 +53,112 @@ class EikonHelper:
             self, 
             region, 
             cell_limit_per_request=100_000, 
-            time_period=12, 
+            time_period=25, 
             request_daily_limit=10_000):      
 
         self._set_app_key()
         self.region = region
         self.cell_limit_per_request = cell_limit_per_request
-        self.instruments = self._get_instruments()
+        self.time_period = time_period
+        self.tickers = self._get_tickers()
         self.fields = self._get_fields()
         self.instrument_limit_per_request = (
             self.cell_limit_per_request // (len(self.fields) * time_period))
         self.data = self._get_data()
-        self.sub_instruments = self._get_sub_instruments()
-        self.batch_instruments = self._get_batch(
-            self.sub_instruments, self.instrument_limit_per_request)
+        self.sub_tickers = self._get_sub_tickers()        
+        self.batch_tickers = self._get_batch(
+            self.sub_tickers, self.instrument_limit_per_request)
         self._get_summary()
-
 
     def _set_app_key(self):
         with open(".config/refinitiv-data.config.json", 'r') as f:
             app_key = json.load(f)['sessions']['platform']['rdp']['app-key']
         ek.set_app_key(app_key)
 
-
     def _get_fields(self):
         with open("assets\metadata\eikon_fields.txt", "r") as f:
             fields = [x.strip("\n") for x in f]
         return fields
 
+    def _get_tickers(self):
+        with open("assets\metadata\eikon_tickers.json", "r") as f:
+            tickers = json.load(f)
+        return tickers
 
-    def _get_instruments(self):
-        with open("assets\metadata\eikon_instruments.json", "r") as f:
-            instruments = json.load(f)
-        return instruments
-
-
-    def _get_schema(self, empty_csv_path):
+    def _get_schema(self, empty_path):
         batch_data, _ = ek.get_data(
-            instruments=self.sub_instruments[0], 
+            self.tickers[0], 
             fields=self.fields,
             parameters={
-                'Scale': 0, 'SDate': 0, 'EDate': -12, 'FRQ': 'FY'})
+                'Scale': 0, 'SDate': 0, 'EDate': -self.time_period, 'FRQ': 'FY'})
 
-        batch_data.columns.values[1] = 'TotAssets Date'
-        batch_data.columns.values[2] = 'PriceClose Date'      
+        batch_data.columns.values[1] = 'TotAssets.Date'
+        batch_data.columns.values[2] = 'Price Close.Date'
+        batch_data.columns.values[3] = 'MktCap.Date'
+        batch_data.columns.values[4] = 'CompanySharesOutstanding.Date'  
         empty_schema = pd.DataFrame(columns=batch_data.columns)
-        empty_schema.to_csv(empty_csv_path, index=False)
+        empty_schema.to_parquet(empty_path)
         return empty_schema
 
-
     def _get_data(self):
-        self.csv_path = os.path.join("assets", "Eikon", f"{self.region.lower()}_data.csv")
-        if os.path.exists(self.csv_path):
-            data = pd.read_csv(self.csv_path, low_memory=False)
+        self.save_path = os.path.join("assets", "Eikon", f"{self.region.lower()}_data.parquet")
+        if os.path.exists(self.save_path):
+            data = pd.read_parquet(self.save_path)
         else:
-            empty_csv_path = os.path.join("assets", "metadata", "empty_eikon_data.csv")
-            if not os.path.exists(empty_csv_path):
-                data = self._get_schema(empty_csv_path)
+            empty_path = os.path.join("assets", "metadata", "empty_eikon_data.parquet")
+            if not os.path.exists(empty_path):
+                data = self._get_schema(empty_path)
             else:
-                data = pd.read_csv(empty_csv_path)
+                data = pd.read_parquet(empty_path)
         return data
 
-
-    def _get_sub_instruments(self):
-        self.retrieved_instruments = self.data['Instrument'].unique().tolist()
-        sub_instruments = [
-            i for i in self.instruments[self.region] 
-            if i not in self.retrieved_instruments]
-        return sub_instruments
-
+    def _get_sub_tickers(self):
+        self.retrieved_tickers = self.data['Instrument'].unique().tolist()
+        sub_tickers = [
+            i for i in self.tickers[self.region] 
+            if i not in self.retrieved_tickers]
+        return sub_tickers
 
     def _get_batch(self, it, size):
         it = iter(it)
         return list(iter(lambda: tuple(islice(it, size)), ()))
-
 
     def _get_summary(self):
         print("=======================================")
         print("Region:", self.region)
         print("Cell limit per request:", self.cell_limit_per_request)
         print("Instrument limit per request:", self.instrument_limit_per_request)
-        print("Instruments retrieved:", len(self.retrieved_instruments))
-        print("Instruments remaining:", len(self.sub_instruments)) 
+        print(f"Tickers retrieved: {len(self.retrieved_tickers)}/{len(self.tickers[self.region])}")
         print("=======================================")
 
-
     def extract(self):
-        if not self.batch_instruments:
+        if not self.batch_tickers:
             logger.info(f"{self.region} completed")
             return
 
-        for instruments in tqdm(self.batch_instruments):
+        for tickers in tqdm(self.batch_tickers):
             try:
                 batch_data, _ = ek.get_data(
-                    instruments=list(instruments),
+                    list(tickers),
                     fields=self.fields,
-                    parameters={'Scale': 0, 'SDate': 0, 'EDate': -12, 'FRQ': 'FY'})
+                    parameters={'Scale': 0, 'SDate': 0, 'EDate': -self.time_period, 'FRQ': 'FY'})
 
-                batch_data.columns.values[1] = 'TotAssets Date'
-                batch_data.columns.values[2] = 'PriceClose Date'  
+                batch_data.columns.values[1] = 'TotAssets.Date'
+                batch_data.columns.values[2] = 'Price Close.Date'
+                batch_data.columns.values[3] = 'MktCap.Date'
+                batch_data.columns.values[4] = 'CompanySharesOutstanding.Date'
+                if 'Instrument' not in batch_data.columns.tolist():
+                    logger.info(f"Empty dataframe: {tickers}")
+
+                self.data = pd.concat([self.data, batch_data])
+                self.data['CUSIP Code'] = self.data['CUSIP Code'].astype(str)
+                self.data.to_parquet(self.save_path)
+                time.sleep(1)
+
             except Exception as e:
+                print(e)
                 continue
 
-            if 'Instrument' not in batch_data.columns.tolist():
-                logger.info(f"Empty dataframe: {instruments}")
-
-            self.data = pd.concat([self.data, batch_data])
-            self.data.to_csv(self.csv_path, index=False)
-            time.sleep(10)
 
 if __name__ == "__main__":
     eikon_helper = EikonHelper(region="Europe")
